d6f6eea0ccf46e13a4f2e990486a587c2fde27a7
David Ko <sjkoh09@gmail.com>
2023-08-01 16:19:09 +0900

    Lost&Win 창 구현

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 "use strict"
 
 
-
-//타이머
+//타이머***********
 //setInterval(함수, 딜레이)
 //parseInt
 //clearInterval()
+//lost 창 호출 구현
 let setTime = 10;
+const lostWindow = document.querySelector('#lostWindow');
 const timer = () => {
     const timerInterval = setInterval(() => {
         if(setTime === 10){
@@ -18,40 +19,42 @@ const timer = () => {
         setTime--;
         if(setTime < 0) {
             clearInterval(timerInterval);
-            //lost 창 호출
+            lostWindow.setAttribute("style", "display: block");
         }
     }, 1000);
 }
 
 
-
-
-
-
-//스타트버튼 
+//스타트버튼***********
 //버튼이 pause로 바뀜
 //카운터 함수 호출
 //타이머 함수 호출
 //벌레 무작위로 뿌리기
 //당근 무작위로 뿌리기
-const startBtn = document.querySelector('.startBtn');
+const startBtn = document.querySelector('#startBtn');
 startBtn.addEventListener('click', timer);
 
 
 
-//카운터
+//카운터***********
 let carrotNumber = "";
 
 
+//당근누르기 (lost 또는 win 창 뜨면 누르는 것 비활성화 되어야 함)
+
+//벌레누르기 (lost 또는 win 창 뜨면 누르는 것 비활성화 되어야 함)
 
 
 
-//Pause
+
+//Pause***********
 
 
-//Lost
-
-//Win
+//Lost***********
 
 
-//Resume
+
+//Win***********
+
+
+//Resume***********
